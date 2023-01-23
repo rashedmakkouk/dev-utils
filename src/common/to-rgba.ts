601a220047af3366d8f49bae8b06f147adaea35c
Rashed Makkouk <rashed.makkouk@outlook.com>
2023-01-23 18:04:12 +0200

    chore: Refactor 'toRGBa' helper implementation
    - Handle invalid 'color' value
    - Update 'color' type assignment
    - Apply supplied 'alpha' for fallback color on invalid value
    - Update function, params and return description

--- src/common/to-rgba.ts
+++ src/common/to-rgba.ts
@@ -1,24 +1,27 @@
 /** Utilities */
-import colorConvert from 'color-convert';
-
+import { hex, keyword } from 'color-convert';
 /** Typings */
 import { KEYWORD } from 'color-convert/conversions';
+import colors from 'color-name';
 
 /**
- * Converts color from keyword or hex to RGBa value.
+ * Converts color from 'keyword' (e.g. green) or 'hex' (e.g. #00FF00) to RGBa
+ * value. Useful when there is a need to apply color opacity.
+ *
+ * @param color - Color `Name` or `HEX` value.
+ * @param alpha - Alpha channel `opacity` between '0' and '1'; default 1.
+ *
+ * @returns RGBa value for valid colors else 'rgba(0,0,0,alpha)'.
  */
 function toRGBa(color: KEYWORD | string, alpha = 1): string {
-  if (!color) {
-    return 'rgba(0,0,0,0)';
-  }
+  const numbers = color.startsWith('#')
+    ? hex.rgb(color)
+    : keyword.rgb(color as keyof typeof colors);
 
-  const numbers = !color.startsWith('#')
-    ? colorConvert.keyword.rgb(color as KEYWORD)
-    : colorConvert.hex.rgb(color);
+  !numbers.length && numbers.push(0, 0, 0);
 
-  return `rgba(${numbers.map((num): number => num)},${
-    alpha == null ? 1 : alpha
-  })`;
+  /* eslint-disable-next-line @typescript-eslint/restrict-template-expressions */
+  return `rgba(${numbers.map((num): number => num)},${alpha ?? 1})`;
 }
 
 export default toRGBa;
